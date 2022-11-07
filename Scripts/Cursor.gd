3d6122b1093b4ef02d15db0add62c8e3b7247a97
kennethbchen <kennethbchen@gmail.com>
2022-11-06 22:45:21 -0500

    Attempting to implement placing blocks

--- Scripts/Cursor.gd
+++ Scripts/Cursor.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 export var cursor_speed = 10
 
+var target_pos = Vector2.ZERO
+var target_rot: int = 0 # In Degrees
+
 onready var pos_tween = $PosTween
 onready var rot_tween = $RotTween
 
@@ -13,21 +16,24 @@ func _ready():
 
 
 func _process(delta):
-	print(rotation_degrees)
 	pass
 	
 func set_target_pos(new_pos : Vector2):
 	
-	pos_tween.interpolate_property(self, "position", position, new_pos, 0.25, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	if target_pos == new_pos: return
+	
+	target_pos = new_pos
+	pos_tween.interpolate_property(self, "position", position, target_pos, 0.25, Tween.TRANS_CUBIC, Tween.EASE_OUT)
 	pos_tween.start()
 	
 func set_target_rot(new_rot):
 	
 	if rot_tween.is_active(): return
+	if target_rot == new_rot: return
 	
+	target_rot = new_rot
 	
-	
-	rot_tween.interpolate_property(self, "rotation_degrees", rotation_degrees, new_rot, 0.1, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	rot_tween.interpolate_property(self, "rotation_degrees", rotation_degrees, target_rot, 0.1, Tween.TRANS_CUBIC, Tween.EASE_OUT)
 	rot_tween.start()
 	
 
--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -12,10 +12,15 @@ onready var cursor = $Cursor
 
 onready var board = $Board
 
+var current_block: Node2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	root = get_tree().root
+	
+	current_block = load("res://Blocks/T-Piece.tscn").instance()
+	cursor.add_child(current_block)
 	
 	pass # Replace with function body.
 
@@ -35,7 +40,50 @@ func _input(event):
 		
 		if !event.pressed: return
 		
+		if event.button_index == 1:
+			if current_block == null: return
+			
+			var rotated_data = current_block.data
+			var rotated_center = current_block.center + Vector2(1,1)
+			
+			if cursor.target_rot % 360 == 0:
+				pass
+			elif cursor.target_rot % 360 == 90:
+				pass
+			elif cursor.target_rot % 360 == 180:
+				pass
+			elif cursor.target_rot % 360 == 270:
+				pass
+				
+			for row in len(rotated_data):
+					for col in len(rotated_data[row]):
+						if rotated_data[row][col] == 1:
+							var array_coord = (Vector2(col,row) + Vector2(1,1))
+							print(array_coord)
+							print(rotated_center)
+							print(array_coord - rotated_center)
+							print()
+							var tile_pos = board.world_to_map(cursor.position) + (array_coord - rotated_center)
+							set_tile(tile_pos)
+							
+			cursor.set_target_rot(0)
+			
 		if event.button_index == 2:
 			cursor.set_target_rot(cursor.rotation_degrees + 90)
+			
 		
-	
+		
+func set_tile(tilemap_position: Vector2):
+	board.set_cellv(tilemap_position, 0)
+
+
+# Rotates array 90 degrees clockwise, assuming that array is a full rectangle (all rows same size)
+# https://godotengine.org/qa/92697/how-can-i-rotate-an-array-of-array
+func rotate_array(arr) -> Array:
+	var new_arr = []
+	for i in range(len(arr[0])):
+		var row = []
+		for j in range(len(arr)):
+			row.append(arr[len(arr) - j - 1][i])
+		new_arr.append(row)
+	return new_arr
