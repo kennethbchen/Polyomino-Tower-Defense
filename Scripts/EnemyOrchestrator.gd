ee08a443b4a7ff6a24b9e07d878c7142e43dec88
kennethbchen <kennethbchen@gmail.com>
2022-11-17 15:34:27 -0500

    Made enemy spawn counter reset automatically instead of stopping

--- Scripts/EnemyOrchestrator.gd
+++ Scripts/EnemyOrchestrator.gd
@@ -11,11 +11,9 @@ var enemy_end = Vector2(1024, 352)
 
 # Wave Status Strings
 var wave_countdown_message = "Next Wave In: %ss."
-var wave_progressing_message = "Wave in progress..."
-var wave_complete_message = "Wave Complete!"
 
 # Wave Parameters
-var wave_cooldown = 5
+var wave_cooldown = 10
 var spawn_delay = 0.5
 var enemies_per_round = 10
 
@@ -29,7 +27,7 @@ signal wave_status_changed(new_time)
 
 
 func _ready():
-	wave_timer.start(wave_cooldown)
+	wave_timer.start(10)
 	
 func _process(delta):
 	
@@ -40,14 +38,13 @@ func _process(delta):
 		
 func _on_wave_timer_timeout():
 
-
 	# Modify Spawn Parameters
+	# -0.001 to avoid wave timer display flickering from n to n-1 in a frame
+	wave_timer.start(wave_cooldown - 0.001)
 	
 	# Start Spawning Enemies
 	enemies_to_spawn = enemies_per_round
 	spawn_timer.start(spawn_delay)
-	
-	emit_signal("wave_status_changed", wave_progressing_message)
 	
 func _on_spawn_timer_timeout():
 	# Spawn Enemies until the desired number is reached
@@ -64,5 +61,3 @@ func _on_spawn_timer_timeout():
 func _on_enemy_destroyed():
 
 	enemies_alive = max(0, enemies_alive - 1)
-	if enemies_alive <= 0:
-		emit_signal("wave_status_changed", wave_complete_message)
