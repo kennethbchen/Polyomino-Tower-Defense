9f5a57c868d98427ae586349fb51d71eeccda5b6
kennethbchen <kennethbchen@gmail.com>
2022-11-17 15:18:25 -0500

    Implemented enemy spawning

--- Scripts/EnemyOrchestrator.gd
+++ Scripts/EnemyOrchestrator.gd
@@ -5,6 +5,10 @@ export var enemy: PackedScene
 onready var wave_timer = $WaveTimer
 onready var spawn_timer = $SpawnTimer
 
+# Copied from root
+var enemy_start = Vector2(1024, -352)
+var enemy_end = Vector2(1024, 352)
+
 # Wave Status Strings
 var wave_countdown_message = "Next Wave In: %ss."
 var wave_progressing_message = "Wave in progress..."
@@ -17,9 +21,9 @@ var enemies_per_round = 10
 
 # Wave State
 var wave_number = 1
-var time_left_int: int
-var enemies_to_spawn: int
-var enemies_alive: int
+var time_left_int: int = 0
+var enemies_to_spawn: int = 0
+var enemies_alive: int = 0
 
 signal wave_status_changed(new_time)
 
@@ -33,8 +37,6 @@ func _process(delta):
 		time_left_int = int(wave_timer.time_left)
 		emit_signal("wave_status_changed", wave_countdown_message % time_left_int)
 
-	if wave_timer.wait_time <= 0 and enemies_alive == 0:
-		emit_signal("wave_status_changed", wave_complete_message)
 		
 func _on_wave_timer_timeout():
 
@@ -50,12 +52,17 @@ func _on_wave_timer_timeout():
 func _on_spawn_timer_timeout():
 	# Spawn Enemies until the desired number is reached
 	if enemies_to_spawn > 0:
-		print("spawn")
-		
+		var new_enemy = enemy.instance()
+		add_child(new_enemy)
+		new_enemy.init(enemy_start, enemy_end)
+		new_enemy.connect("on_enemy_destroyed", self, "_on_enemy_destroyed")
 		enemies_to_spawn -= 1
 		enemies_alive += 1
 	else:
 		spawn_timer.stop()
 
 func _on_enemy_destroyed():
+
 	enemies_alive = max(0, enemies_alive - 1)
+	if enemies_alive <= 0:
+		emit_signal("wave_status_changed", wave_complete_message)
