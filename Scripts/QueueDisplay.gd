d315fb138c29eda2ddc4ed0d2d07857ed866f117
kennethbchen <kennethbchen@gmail.com>
2022-12-01 14:57:09 -0500

    Generalized waveStateDisplay to StateUIDisplay

--- Scripts/QueueDisplay.gd
+++ Scripts/QueueDisplay.gd
@@ -6,8 +6,6 @@ export var queue_position = 0
 
 onready var preview_sprite = $PreviewImage
 
-var test = load("res://Sprites/Tower.png")
-
 func _on_queue_changed(block_queue):
 	if preview_sprite == null: return
 	preview_sprite.texture = block_queue[queue_position].preview_image
--- Scripts/StateUIDisplay.gd
+++ Scripts/StateUIDisplay.gd
@@ -2,5 +2,5 @@ extends Control
 
 onready var label = $Label
 
-func _on_wave_state_changed(message):
+func _on_state_changed(message):
 	label.text = message
