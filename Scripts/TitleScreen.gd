449de482f520e94a3a18de1b7806ccd456ea6245
kennethbchen <kennethbchen@gmail.com>
2022-12-30 15:29:21 -0500

    Started making tab system for instructions

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -1,10 +1,10 @@
 extends Control
 
-onready var instruction_panel = $InstructionPanel
+onready var instruction_panel = $WindowDialog
 
 func _on_start_game():
 	get_tree().change_scene("res://Game.tscn")
 
 func _on_how_to_play():
-	instruction_panel.popup()
+	instruction_panel.popup_centered()
 
